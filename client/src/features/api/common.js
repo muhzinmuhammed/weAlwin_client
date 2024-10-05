56c3b18887fb290be6ad3dcced454becad94932a
MUHAMMED MUHZIN T S <muhzinsidhiq333@gmail.com>
2024-10-05 19:44:03 +0530

    change base url

--- client/src/features/api/common.js
+++ client/src/features/api/common.js
@@ -1,7 +1,7 @@
 export const all={
-    // baseurl:"http://192.168.1.225:8000/api"
-    baseurl:"http://localhost:8000/api",
-    // baseurl:"https://qurinom-backend-evjl.onrender.com/api",//mozhin
+    
+    // baseurl:"http://localhost:8000/api",
+    baseurl:"https://wealwin-server-2.onrender.com",//mozhin
     
     
 }
